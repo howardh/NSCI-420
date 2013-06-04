62a0e27f7570eaa182b4cd0ebb6ff0ea21dbd257
howardh <howard.huang@mail.mcgill.ca>
2013-06-04 14:54:02 -0400

    Fixed: Two copies of Const.m

--- private/Const.m
+++ private/Const.m
@@ -17,10 +17,23 @@ classdef Const
 	methods
 		function this=Const()
 		end
-
-		%TODO
+	end
+	methods (Static)
 		function ret=ALL_TESTS(expName)
+			%FIXME: Temporary, for debugging purposes
 			ret={'065'};
+			%ret={'137'};
+			return;
+			%Actual function starts here
+			if strcmp(expName, '12mv1211')
+				ret={'024', '025', '033', '034', '035', '038', '042', '043', '044', '045', ... %Insertion 1
+					'046', '048', '049', '050', '053', '054', ... %Insertion 2
+					'056', '057', '058', '059', '060', '061', ... %Insertion 4
+					'062', '063', '065', '066', '067', '068', ... %Insertion 5
+					'143', '144', '145', '146', '147', '148'}; %Insertion 12
+				return;
+			end
+			ret={'000'};
 		end
 	end
 end
