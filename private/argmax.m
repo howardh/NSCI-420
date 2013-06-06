d1bd04f9be3b42611ed50e219fe8c8c9c3ce71d2
howardh <howard.huang@mail.mcgill.ca>
2013-06-06 15:46:59 -0400

    Cleaning up argmax

--- private/argmax.m
+++ private/argmax.m
@@ -1,41 +1,11 @@
 function ret=argmax(matrix)
 	[m i] = max(matrix(:));
-	if isVector(matrix)
+	if isvector(matrix)
 		ret=i;
 	else
-		ret=ind2sub(size(matrix),i);
+		n=ndims(matrix);
+		ret=cell(1,n);
+		[ret{:}]=ind2sub(size(matrix),i);
+		ret=cell2mat(ret);
 	end
-
-	%matrix=matrix(:);
-	%ret=1;
-	%for i=2:length(matrix)
-	%	if (matrix(i) > matrix(ret))
-	%		ret=i;
-	%	end
-	%end
-	%ret=am(matrix);
-end
-
-function ret=am(matrix)
-	dim=size(matrix);
-
-	currMax=1;
-	for i=2:length(matrix)
-		if (matrix(i) > matrix(currMax))
-			currMax=i;
-		end
-	end
-	ret=currMax;
-
-	i=length(dim);
-	while (i >= 1)
-		dim(i)=prod(dim(1:i-1));
-		i=i-1;
-	end
-
-end
-
-function ret=isVector(matrix)
-	dim = size(matrix);
-	ret = dim(1)==1 && length(dim)==2;
 end
