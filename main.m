047b4df16c5dff0ca01d23d8294e21f5cf5bf6a4
howardh <howard.huang@mail.mcgill.ca>
2013-07-05 13:05:16 -0400

    test6

--- main.m
+++ main.m
@@ -2,6 +2,10 @@ function ret=main()
 	addpath(Const.SCRIPT_DIRECTORY);
 	onCleanup(@() cd(Const.SCRIPT_DIRECTORY));
 
+	%dir2 = [Const.RESULT_DIRECTORY pathname('test5', '12mv1211', 'Covariance')];
+	%load([dir2 'results.mat']);
+	%results('044')
+
 	%x=test4;
 	%x.testName='071';
 	%x.stdViewer();
@@ -9,7 +13,22 @@ function ret=main()
 	%x.testName='071';
 	%x.alignmentViewer();
 
-	%run('test5');
+	run('test6');
+	%ret = ClassificationDiscriminant.fit(rand(10,20),[zeros(1,7) ones(1,3)])
+	%load('tsx.mat');
+	%load('tsy.mat');
+	%y = transpose(y);
+
+	%gscatter(x(:,13),x(:,17),y);
+	%legend('non-prefered','prefered');
+
+	%obj = ClassificationDiscriminant.fit(x,y);
+	%%load('vsx.mat');
+	%%load('vsy.mat');
+	%[label,score,cost] = predict(obj,x);
+	%cost
+	%size(y)
+	%size(label)
 
 	%Meeting 2013.06.27
 	%x=test4
@@ -23,7 +42,7 @@ function ret=main()
 	%csd2=loader.load('082');
 	%combineCSD(csd1,csd2);
 
-	createImages();
+	%createImages();
 
 	%runAll();
 end
